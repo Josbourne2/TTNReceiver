7d7e924ca28dcba916f8f763d045d5634baad4ec
Josbourne2 <jos.menhart@live.nl>
2020-10-04 00:57:54 +0200

    fdgdf

---
+++ TTNReceiver.Core/Device.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace TTNReceiver.Core
+{
+    internal class Device
+    {
+        public string Name { get; set; }
+        public string EUI { get; set; }
+        public DeviceType deviceType;
+
+        [ForeignKey]
+        public int DeviceTypeId;
+    }
+}
---
+++ TTNReceiver.Core/Measurement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TTNReceiver.Core
+{
+    internal class Measurement
+    {
+        public int DeviceId { get; set; }
+        public string Name { get; set; }
+        public double Value { get; set; }
+    }
+}
