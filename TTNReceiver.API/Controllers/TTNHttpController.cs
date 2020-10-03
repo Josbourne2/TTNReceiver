913ab2efab063f7ca023d496b3cb4e2af33eebe0
Josbourne2 <jos.menhart@live.nl>
2020-10-04 00:45:19 +0200

    Add project files.

---
+++ TTNReceiver.API/Controllers/TTNHttpController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace TTNReceiver.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TTNHttpController : ControllerBase
+    {
+        public TTNHttpController()
+        {
+        }
+
+        [HttpPost]
+        public void ReceiveMessage([FromBody] object message)//TTNHttpDTO
+        {
+            TTNHttpDTO dinges = JsonConvert.DeserializeObject<TTNHttpDTO>(message.ToString());
+            Console.WriteLine(dinges.payload_raw);
+        }
+    }
+}
